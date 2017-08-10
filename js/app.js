b94765657c056d4c0e09a78246a0f7cf4a442259
robhyrkiel <rob.hyrkiel@gmail.com>
2017-08-10 11:44:02 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //Calculates total students, then divides by 10 to determine pages required
 const students = document.querySelectorAll('.student-item');
 const pages = Math.ceil(students.length/10);
-const pagination = document.querySelector('.pagination ul')
+const pagination = document.querySelector('ul')
 //Shows only first 10 students
 $(students).slice(10).hide();
 
@@ -20,10 +20,13 @@ let showpage = (pageNumber, studentList) => {
 //Creates pagination buttons depending on size of student list
 //Calls showpage function when a pagination button is clicked
 let appendPageLinks = (studentsToShow) => {
-	for (let i=2;i<pages+1;i++) {
+		let div = document.createElement('div');
+		$(div).addClass('pagination');
+		pagination.appendChild(div);
+	for (let i=1;i<pages+1;i++) {
 		let li = document.createElement('li');
 		li.innerHTML = '<a href="#">' + i + '</a>';
-		pagination.appendChild(li);
+		div.appendChild(li);
 
 	}
 	let activeClass = $('.pagination a');
@@ -33,12 +36,51 @@ let appendPageLinks = (studentsToShow) => {
 		let pageNumber = e.target.textContent;
 		showpage(pageNumber, studentsToShow);
 	})
-	
 }
 
 appendPageLinks(students);
 
+//SEARCH FUNCTION
+//Inserts search bar and button
+const h2 =  document.querySelector('h2');
+h2.insertAdjacentHTML('afterend', "<div class='student-search'><input placeholder='Search for students...'><button>Search</button></div>");
+
+//Captures input field
+const inputField = document.querySelector('input');
+const myButton = document.getElementsByTagName('button')[0];
+
+//The following arrays are used to search through names and emails when search button is clicked
+let studentNames = [];
+let studentEmails = [];
+
+//When search button is clicked the following code runs
+myButton.addEventListener('click', () => {
+		$(students).hide();
+		$(".pagination").hide();
+		let searchTerm = inputField.value;
+		let searchList = document.querySelector('.student-list');
+		//All student names and emails are put into separate arrays
+		for (let i=0;i<students.length;i++) {	
+			studentNames.push(students[i].querySelector('h3').innerHTML);
+			studentEmails.push(students[i].querySelector('.email').innerHTML);
+			console.log(studentNames);			
+			}
+		//Searches array for results for names and emails
+		let nameResults = studentNames.indexOf(searchTerm);
+		let emailResults = studentEmails.indexOf(searchTerm);;
+		// If search finds no results display message, otherwise display results//
+		if (nameResults !== -1 || emailResults !== -1 ) {
+			$(".results").hide();
+			$(students[nameResults]).show();
+			$(students[emailResults]).show();
+		} else {
+			const div = document.querySelector('.page-header');
+			div.insertAdjacentHTML('afterend', "<p class='results'>No such result. Please try again.</p>");
+		}
+		})
 	
+
+
 
 
 
